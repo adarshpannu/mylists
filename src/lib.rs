9abb480782774a6d8fa051e4d660d1a7b1f5228e
Adarsh Pannu <13154869+adarshpannu@users.noreply.github.com>
2020-06-30 19:04:56 -0700

    Basic SLL with push+pop, all over again

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,56 @@
 #![allow(warnings)]
 
+#[derive(Debug)]
 struct List<T> {
     head: NodePtr<T>,
 }
 
-type NodePtr<T> = Option<Box<Node<T>>>;
+impl<T> List<T> {
+    fn new() -> List<T> {
+        List { head: None }
+    }
 
-struct Node<T> {
-    elem: T,
-    next: NodePtr<T>
+    fn push(&mut self, elem: T) {
+        let mut new_node = Node { elem, next: None };
+        self.head
+            .take()
+            .map(|prev_head| new_node.next = Some(prev_head));
+        self.head = Some(Box::new(new_node));
+    }
+
+    fn pop(&mut self) -> Option<T> {
+        let prev_head = self.head.take();
+        prev_head.map(|prev_node| {
+            self.head = prev_node.next;
+            prev_node.elem
+        })
+    }
 }
 
-#[test]
-fn test() {
+type NodePtr<T> = Option<Box<Node<T>>>;
 
+#[derive(Debug)]
+struct Node<T> {
+    elem: T,
+    next: NodePtr<T>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn basic_push_pop() {
+        let mut lst: List<i32> = List::new();
+        let elems = vec![-10, 27, 5, 2, 0, 11];
+        for &elem in elems.iter() {
+            lst.push(elem);
+        }
+
+        for &elem in elems.iter().rev() {
+            assert_eq!(lst.pop(), Some(elem));
+        }
+        assert_eq!(lst.pop(), None);
+        assert_eq!(lst.pop(), None);
+    }
 }
