4db0e06793476760e49d7b2121290a2bbde68343
Adarsh Pannu <13154869+adarshpannu@users.noreply.github.com>
2020-06-30 18:43:42 -0700

    Restart

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,17 @@
+#![allow(warnings)]
+
+struct List<T> {
+    head: NodePtr<T>,
+}
+
+type NodePtr<T> = Option<Box<Node<T>>>;
+
+struct Node<T> {
+    elem: T,
+    next: NodePtr<T>
+}
+
 #[test]
 fn test() {
-    
+
 }
