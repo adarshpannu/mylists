5c47952770b6cbd058762bd9c7ca7d2d00af8d6c
Adarsh Pannu <13154869+adarshpannu@users.noreply.github.com>
2020-06-30 23:33:53 -0700

    SLL push/pop/peek

--- src/lib.rs
+++ src/lib.rs
@@ -6,24 +6,27 @@ struct List<T> {
 }
 
 impl<T> List<T> {
-    fn new() -> List<T> {
+    fn new() -> Self {
         List { head: None }
     }
 
     fn push(&mut self, elem: T) {
-        let mut new_node = Node { elem, next: None };
-        self.head
-            .take()
-            .map(|prev_head| new_node.next = Some(prev_head));
+        let mut new_node = Node {
+            elem,
+            next: self.head.take(),
+        };
         self.head = Some(Box::new(new_node));
     }
 
     fn pop(&mut self) -> Option<T> {
-        let prev_head = self.head.take();
-        prev_head.map(|prev_node| {
+        self.head.take().map(|prev_node| {
             self.head = prev_node.next;
             prev_node.elem
         })
+    }
+
+    fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|noderef| &noderef.elem)
     }
 }
 
@@ -40,17 +43,21 @@ mod tests {
     use super::*;
 
     #[test]
-    fn basic_push_pop() {
+    fn basic_push_pop_peek() {
         let mut lst: List<i32> = List::new();
         let elems = vec![-10, 27, 5, 2, 0, 11];
+
         for &elem in elems.iter() {
             lst.push(elem);
         }
 
         for &elem in elems.iter().rev() {
+            assert_eq!(lst.peek(), Some(&elem));
             assert_eq!(lst.pop(), Some(elem));
         }
+
         assert_eq!(lst.pop(), None);
         assert_eq!(lst.pop(), None);
+        assert_eq!(lst.peek(), None);
     }
 }
